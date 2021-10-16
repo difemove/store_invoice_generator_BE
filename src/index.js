9c25bcfc6b0192cd265f331234420e18fadb5978
Diego Mora <difemove@gmail.com>
2021-10-16 10:52:44 -0500

    vercel push

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ const port = 3000;
 
 app.use(express.json())
 
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/products', (req, res) => res.send({
+    products: [
+        {
+            id: 1,
+            name: 'Product 1'
+        }
+    ]
+}));
 app.listen(port,() => {
     console.log (`Server listening on port ${port}`)
 })
